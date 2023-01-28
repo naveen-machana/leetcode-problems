85c2acb633b1f213635a6f3cad12ec62d9b84b65
Naveen Machana <naveen.machana@gmail.com>
2023-01-28 13:40:17 +0530

    743 optimal

--- src/main/java/com/naveen/problems/problem743.java
+++ src/main/java/com/naveen/problems/problem743.java
@@ -1,9 +1,6 @@
 package com.naveen.problems;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 // Dijkstra's shortest path algorithm
 // https://leetcode.com/problems/network-delay-time/
@@ -19,37 +16,29 @@ public class problem743 {
         System.out.println(networkDelayTime(times, 4, 2));
     }
     public static int networkDelayTime(int[][] times, int n, int k) {
-        List<List<List>> g = new ArrayList<>(n + 1);
-        for (int i = 0; i <= n; i++) g.add(new ArrayList<>());
-        for (int[] e : times) {
-            List<Integer> edW = List.of(e[1], e[2]);
-            g.get(e[0]).add(edW);
-        }
-        int[] distance = new int[n + 1];
-        Arrays.fill(distance, Integer.MAX_VALUE);
-        distance[k] = 0;
+        Map<Integer, Map<Integer, Integer>> g = new HashMap<>();
+        for (int[] e : times)
+            g.computeIfAbsent(e[0], key -> new HashMap<>()).put(e[1], e[2]);
+
+        boolean[] visited = new boolean[n + 1];
         PriorityQueue<Node> pq = new PriorityQueue<>();
         pq.offer(new Node(k, 0));
-        boolean[] visited = new boolean[n + 1];
 
-        int maxTimeToReachAllNodes = -1;
-        int count = 0;
+        int max = -1;
         while (!pq.isEmpty()) {
             Node cur = pq.poll();
-            if (visited[cur.v]) continue;
-            visited[cur.v] = true;
-            maxTimeToReachAllNodes = Math.max(maxTimeToReachAllNodes, cur.w);
-            count++;
-            for (List<Integer> adj : g.get(cur.v)) {
-                int v = adj.get(0), w = adj.get(1);
-                if (!visited[v]) {
-                    distance[v] = Math.min(distance[v], distance[cur.v] + w);
-                    pq.offer(new Node(v, distance[v]));
-                }
+            int curw = cur.w, curv = cur.v;
+            if (visited[curv]) continue;
+            visited[curv] = true;
+            max = curw;
+            n--;
+            if (g.containsKey(curv)) {
+                for (int next : g.get(curv).keySet())
+                    pq.offer(new Node(next, curw + g.get(curv).get(next)));
             }
         }
 
-        return count == n ? maxTimeToReachAllNodes : -1;
+        return 0 == n ? max : -1;
     }
 
     private static class Node implements Comparable<Node> {
