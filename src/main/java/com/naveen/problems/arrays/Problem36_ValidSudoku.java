9197b7ccd15a835702356804ff2f6f993383beee
Naveen Machana <naveen.machana@gmail.com>
2023-02-27 06:59:27 +0530

    description for sudoku problem

--- src/main/java/com/naveen/problems/arrays/Problem36_ValidSudoku.java
+++ src/main/java/com/naveen/problems/arrays/Problem36_ValidSudoku.java
@@ -3,6 +3,20 @@ package com.naveen.problems.arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+/*
+* https://leetcode.com/problems/valid-sudoku/description/
+* Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following
+* rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+* */
+
 public class Problem36_ValidSudoku {
     public boolean isValidSudoku(char[][] board) {
         Set<String> seen = new HashSet<>();
