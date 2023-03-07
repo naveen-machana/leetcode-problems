e053b7d601e99461bd8affc938fef47d9db7c304
Naveen Machana <naveen.machana@gmail.com>
2023-03-07 06:01:32 +0530

    refactoring

--- src/main/java/com/naveen/problems/arrays/Problem229_MajorityElementII.java
+++ src/main/java/com/naveen/problems/arrays/Problem229_MajorityElementII.java
@@ -4,6 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 // https://leetcode.com/problems/majority-element-ii/
+/*
+Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+Example 1:
+Input: nums = [3,2,3]
+Output: [3]
+Example 2:
+
+Input: nums = [1]
+Output: [1]
+Example 3:
+
+Input: nums = [1,2]
+Output: [1,2]
+* */
 public class Problem229_MajorityElementII {
     public static void main(String[] args) {
         Problem229_MajorityElementII s = new Problem229_MajorityElementII();
