83b7d77391f225d42a0a6a8fc7c4b92a3e121280
Naveen Machana <naveen.machana@gmail.com>
2023-03-07 07:28:29 +0530

    refactoring

--- src/main/java/com/naveen/problems/arrays/Problem493_ReversePairs.java
+++ src/main/java/com/naveen/problems/arrays/Problem493_ReversePairs.java
@@ -1,5 +1,7 @@
 package com.naveen.problems.arrays;
 
+import java.util.LinkedList;
+
 // https://leetcode.com/problems/reverse-pairs/
 // 493. Reverse Pairs
 // Given an integer array nums, return the number of reverse pairs in the array.
@@ -17,44 +19,46 @@ public class Problem493_ReversePairs {
     public static void main(String[] args) {
         Problem493_ReversePairs s = new Problem493_ReversePairs();
         int[] a = {1,3,2,3,1};
-        s.reversePairs(a);
+        System.out.println(s.reversePairs(a));
     }
     public int reversePairs(int[] nums) {
         return mergesort(nums, 0, nums.length - 1);
     }
 
-    private int mergesort(int[] a, int st, int end) {
+    private int mergesort(int[] nums, int st, int end) {
         if (st >= end) return 0;
         int mid = (st + end)/2;
-        int l = mergesort(a, st, mid);
-        int r = mergesort(a, mid + 1, end);
-        int m = merge(a, st, mid, end);
-        return l + r + m;
+        int left = mergesort(nums, st, mid);
+        int right = mergesort(nums, mid + 1, end);
+        int merged = merge(nums, st, mid, end);
+        return left + right + merged;
     }
 
-    private int merge(int[] a, int l, int m, int r) {
-        int count = 0, j = m + 1;
-        for (int i = l; i <= m; i++) {
-            while (j <= r && a[i] > (long)2 * a[j]) j++;
-            count += (j - m - 1);
+    private int merge(int[] nums, int st, int mid, int end) {
+        int k1 = st, k2 = mid + 1, count = 0;
+        for (; k1 <= mid; k1++) {
+            while (k2 <= end && nums[k1] > (long)2 * nums[k2]) {
+                count += (mid - k1 + 1);
+                k2++;
+            }
+        }
+        k1 = st; k2 = mid + 1;
+
+        LinkedList<Integer> merged = new LinkedList<>();
+        while (k1 <= mid && k2 <= end) {
+            if (nums[k1] <= nums[k2])
+                merged.addLast(nums[k1++]);
+            else {
+                merged.addLast(nums[k2++]);
+            }
         }
 
-        int n1 = m - l + 1, n2 = r - m;
-        int[] left = new int[n1];
-        int[] right = new int[n2];
-        int i = 0, k = l;
-        for (; i < n1; i++) left[i] = a[l + i];
-        for (j = 0; j < n2; j++) right[j] = a[j + m + 1];
-
-        i = 0; j = 0;
-        while (i < n1 && j < n2) {
-            if (left[i] <= right[j])
-                a[k++] = left[i++];
-            else
-                a[k++] = right[j++];
+        while (k1 <= mid) merged.addLast(nums[k1++]);
+        while (k2 <= end) merged.addLast(nums[k2++]);
+        int i = st;
+        for (int ele : merged) {
+            nums[i++] = ele;
         }
-        while (i < n1) a[k++] = left[i++];
-        while (j < n2) a[k++] = right[j++];
         return count;
     }
 }
