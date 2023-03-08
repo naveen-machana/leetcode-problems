5e96c022b3382ea0d7d1a2547c65b0109a5ad9f2
Naveen Machana <naveen.machana@gmail.com>
2023-03-08 16:00:53 +0530

    practice

--- src/main/java/com/naveen/problems/arrays/Problem42_TrappingRainWater.java
+++ src/main/java/com/naveen/problems/arrays/Problem42_TrappingRainWater.java
@@ -10,35 +10,34 @@ public class Problem42_TrappingRainWater {
     public int trap1(int[] a) {
         int n = a.length;
         int[] left = new int[n], right = new int[n];
+        int res = 0;
 
         left[0] = a[0]; right[n - 1] = a[n - 1];
-        for (int i = 1; i < n; i++) left[i] = Math.max(left[i - 1], a[i]);
-        for (int i = n - 2; i >= 0; i--) right[i] = Math.max(a[i], right[i + 1]);
+        for (int i = 1; i < n; i++)  left[i] = Math.max(left[i - 1], a[i]);
+        for (int j = n - 2; j >= 0; j--) right[j] = Math.max(right[j + 1], a[j]);
 
-        int res = 0;
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) {
             res += Math.min(left[i], right[i]) - a[i];
+        }
 
         return res;
     }
 
     public int trap(int[] a) {
-        int n = a.length;
+        int n = a.length, res = 0;
         ArrayDeque<Integer> stack = new ArrayDeque<>();
 
-        int i = 0, maxWater = 0, maxBotWater;
-        while (i < n) {
-            if (stack.isEmpty() || a[stack.peek()] <= a[i]) {
-                stack.push(i++);
-            }
+        for (int i = 0; i < n; i++) {
+            if (stack.isEmpty() || a[stack.peek()] > a[i]) stack.push(i);
             else {
-                int bot = stack.pop();
-                maxBotWater = stack.isEmpty() ? 0
-                        : (Math.min(a[stack.peek()], a[i]) - a[bot])
-                          * (i - stack.peek() - 1);
-                maxWater += maxBotWater;
+                while (!stack.isEmpty() && a[stack.peek()] <= a[i]) {
+                    int barIndex = stack.pop();
+                    res += stack.isEmpty() ? 0 : (i - stack.peek() - 1) * (Math.min(a[i], a[stack.peek()]) - a[barIndex]);
+                }
+                stack.push(i);
             }
         }
-        return maxWater;
+
+        return res;
     }
 }
