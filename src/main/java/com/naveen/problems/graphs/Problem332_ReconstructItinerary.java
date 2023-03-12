6e1b0417dd1d41044ac2791f1e46533b22f38f70
Naveen Machana <naveen.machana@gmail.com>
2023-03-12 21:25:43 +0530

    refactoring

--- src/main/java/com/naveen/problems/graphs/Problem332_ReconstructItinerary.java
+++ src/main/java/com/naveen/problems/graphs/Problem332_ReconstructItinerary.java
@@ -2,6 +2,7 @@ package com.naveen.problems.graphs;
 
 import java.util.*;
 
+// https://leetcode.com/problems/reconstruct-itinerary/
 // 332. Reconstruct Itinerary
 // You are given a list of airline tickets where tickets[i] = [fromi, toi] represent the departure and the arrival
 // airports of one flight. Reconstruct the itinerary in order and return it.
@@ -12,26 +13,25 @@ import java.util.*;
 //
 // For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
 // You may assume all tickets form at least one valid itinerary. You must use all the tickets once and only once.
-// Solution: Eulerian Path and Eulerian Circuit
+// Solution: Eulerian Path and Eulerian Circuit - If both exists in a graph then its a euler graph
 // Eulerian Path: If Euler Path exists, then you should be able to visit all edges exactly once
 // Eulerian Circuit: If Euler Circuit exists, then you should be able to visit all edges exactly once and returned back
 // source vertex.
 class Problem332_ReconstructItinerary {
     public List<String> findItinerary(List<List<String>> tickets) {
         Map<String, PriorityQueue<String>> g = new HashMap<>();
-        for (List<String> e : tickets)
-            g.computeIfAbsent(e.get(0), k -> new PriorityQueue<>()).offer(e.get(1));
+        for (List<String> tkt : tickets)
+            g.computeIfAbsent(tkt.get(0), k -> new PriorityQueue<>()).offer(tkt.get(1));
 
-        List<String> res = new LinkedList<>();
+        LinkedList<String> res = new LinkedList<>();
         visit("JFK", g, res);
-
         return res;
     }
 
-    private void visit(String src, Map<String, PriorityQueue<String>> g, List<String> res) {
+    private void visit(String src, Map<String, PriorityQueue<String>> g, LinkedList<String> res) {
         while (g.containsKey(src) && !g.get(src).isEmpty())
             visit(g.get(src).poll(), g, res);
 
-        res.add(0, src);
+        res.addFirst(src);
     }
 }
