fe955379d7c7c4404dd5de291cecd320b2e17f71
Naveen Machana <naveen.machana@gmail.com>
2023-03-14 05:28:00 +0530

    refactoring

--- src/main/java/com/naveen/problems/graphs/Problem1042_FlowPlantingWithNoAdjacent.java
+++ src/main/java/com/naveen/problems/graphs/Problem1042_FlowPlantingWithNoAdjacent.java
@@ -1,8 +1,9 @@
-package com.naveen.problems.graphs.problem1042;
+package com.naveen.problems.graphs;
 
 import java.util.ArrayList;
 import java.util.List;
 
+// https://leetcode.com/problems/flower-planting-with-no-adjacent/
 // 1042. Flower Planting With No Adjacent
 // You have n gardens, labeled from 1 to n, and an array paths where paths[i] = [xi, yi] describes a bidirectional
 // path between garden xi to garden yi. In each garden, you want to plant one of 4 types of flowers.
@@ -14,23 +15,20 @@ import java.util.List;
 //
 //Return any such a choice as an array answer, where answer[i] is the type of flower planted in the (i+1)th garden.
 // The flower types are denoted 1, 2, 3, or 4. It is guaranteed an answer exists.
-public class Solution {
+public class Problem1042_FlowPlantingWithNoAdjacent {
     public int[] gardenNoAdj(int n, int[][] paths) {
         List<List<Integer>> g = new ArrayList<>(n);
         for (int i = 0; i < n; i++) g.add(new ArrayList<>());
-        for (int[] path : paths) {
-            g.get(path[0] - 1).add(path[1] - 1);
-            g.get(path[1] - 1).add(path[0] - 1);
+        for (int[] p : paths) {
+            g.get(p[0] - 1).add(p[1] - 1);
+            g.get(p[1] - 1).add(p[0] - 1);
         }
         int[] res = new int[n];
         for (int i = 0; i < n; i++) {
             boolean[] taken = new boolean[5];
             for (int adj : g.get(i)) taken[res[adj]] = true;
-            for (int j = 1; j < taken.length; j++)
-                if (!taken[j]) {
-                    res[i] = j;
-                    break;
-                }
+            for (int j = 1; j < 5; j++)
+                if (!taken[j]) res[i] = j;
         }
         return res;
     }
