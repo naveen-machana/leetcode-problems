8e8ad645ca2403f75f9a2af98f8557e8e1e01f82
Naveen Machana <naveen.machana@gmail.com>
2023-03-28 22:03:51 +0530

    practice

---
+++ src/main/java/com/naveen/problems/arrays/Problem48_RotateImage.java
@@ -0,0 +1,19 @@
+package com.naveen.problems.arrays;
+
+public class Problem48_RotateImage {
+    public void rotate(int[][] a) {
+        int n = a.length;
+        int l = 0, r = n - 1;
+        while (l < r) {
+            for (int i = 0; i < r - l; i++) {
+                int top = l, bottom = r;
+                int topleft = a[top][l + i];
+                a[top][l + i] = a[bottom - i][l];
+                a[bottom - i][l] = a[bottom][r - i];
+                a[bottom][r - i] = a[top + i][r];
+                a[top + i][r] = topleft;
+            }
+            l++; r--;
+        }
+    }
+}
