2642484dab2d12a88ee38ebe3b9d06e6f3f61351
Naveen Machana <naveen.machana@gmail.com>
2023-04-16 13:26:09 +0530

    practice

--- src/main/java/com/naveen/problems/graphs/Problem130_SurroundedRegions.java
+++ src/main/java/com/naveen/problems/graphs/Problem130_SurroundedRegions.java
@@ -8,43 +8,34 @@ import java.util.Arrays;
 //
 // A region is captured by flipping all 'O's into 'X's in that surrounded region.
 public class Problem130_SurroundedRegions {
-    public static void main(String[] args) {
-        char[][] board = {{'X','O','X'},{'X','O','X'},{'X','O','X'}};
-        Problem130_SurroundedRegions s = new Problem130_SurroundedRegions();
-        s.solve(board);
-
-        System.out.println(Arrays.toString(board));
-    }
-    int[][] MOVES = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
     public void solve(char[][] board) {
         int m = board.length, n = board[0].length;
         boolean[][] visited = new boolean[m][n];
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 'O' && !visited[i][j] && (i == 0 || i == m - 1 || j == 0 || j == n - 1)) {
-                    dfs(board, i, j, m, n, visited);
-                }
-            }
+        for (int c = 0; c < n; c++) {
+            if (!visited[0][c] && board[0][c] == 'O') dfs(0, c, m, n, visited, board);
+            if (!visited[m - 1][c] && board[m - 1][c] == 'O') dfs(m - 1, c, m, n, visited, board);
         }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visited[i][j])
-                    board[i][j] = 'X';
-            }
+        for (int r = 0; r < m; r++) {
+            if (!visited[r][0] && board[r][0] == 'O') dfs(r, 0, m, n, visited, board);
+            if (!visited[r][n - 1] && board[r][n - 1] == 'O') dfs(r, n - 1, m, n, visited, board);
         }
+
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (!visited[i][j]) board[i][j] = 'X';
     }
 
-    void dfs(char[][] board, int r, int c, int m, int n, boolean[][] visited) {
-        visited[r][c] = true;
+    private void dfs(int i, int j, int m, int n, boolean[][] visited, char[][] board) {
+        if (i < 0 || j < 0 || i >= m || j >= n
+            || visited[i][j]
+            || board[i][j] == 'X') return;
 
-        for (int[] move : MOVES) {
-            int nextr = move[0] + r, nextc = move[1] + c;
-            if (nextr >= 0 && nextr < m && nextc >= 0 && nextc < n
-                    && !visited[nextr][nextc] && board[nextr][nextc] == 'O') {
-                dfs(board, nextr, nextc, m, n, visited);
-            }
-        }
+        visited[i][j] = true;
+        dfs(i + 1, j, m, n, visited, board);
+        dfs(i - 1, j, m, n, visited, board);
+        dfs(i, j + 1, m, n, visited, board);
+        dfs(i, j - 1, m, n, visited, board);
     }
 }
